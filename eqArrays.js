d9a3d434dcd014e95e4860e895f906c4ced839b7
Manuel Casanova <manucasanova@hotmail.com>
2022-01-29 12:20:39 -0800

    comments added

--- eqArrays.js
+++ eqArrays.js
@@ -12,14 +12,14 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) { //Checks if the length of arrays is the same. Obviously, this is the first thing we should check. If it's not the same, they are no equal, it returns false and it does not run anything else.
-    return false
+    return false;
   }
   for (let i = 0; i < array1.length; i++) { //Now we loop through array1. Why array1? Well, both arrays have the same length.
-if (array1[i] !== array2[i]) {//with this loop they compare each index of array1 with equivalent idex of array2. If they are not equal returns false
-  return false
-}
+    if (array1[i] !== array2[i]) {//with this loop they compare each index of array1 with equivalent idex of array2. If they are not equal returns false
+      return false;
+    }
   }
-  return true //otherwise returns true.
+  return true; //otherwise returns true.
 };
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); //should pass
