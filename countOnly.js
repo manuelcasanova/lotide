cefcd172ecf3cd3a37df5f7cae68773d35768335
Manuel Casanova <manucasanova@hotmail.com>
2022-01-31 12:59:58 -0800

    middle cleaned up, countOnly first commit

---
+++ countOnly.js
@@ -0,0 +1,62 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`\uD83D\uDFE2 \uD83D\uDFE2 Assertion Passed: ${actual} === ${expected}!`);
+  } else {
+    console.log(`\uD83D\uDD34 \uD83D\uDD34 Assertion Failed: ${actual} !== ${expected}!`);
+  }
+};
+
+//Above: assertEqual function
+
+/*
+We will be given an array. For example: "a, a, b, c, d, e, a"
+We will be given an object. For example: {a: true, d: true, b: false, f: true}
+We want it to return a: 3, d: 1. Why not b? Because "false". Why not e? Because not in object. Why not f? Because not in array
+*/
+
+
+//Below: COUNT ONLY
+
+//The function should report back how many instances of each string were found in the allItems array of strings. We want it to report an object.
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+      results[item] += 1;
+      } else {
+      results[item] = 1;
+      }
+    }
+  }
+
+  return results;
+}
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+ 
+  console.log(assertEqual(result1["Jason"], 1));
+  console.log(assertEqual(result1["Karima"], undefined));
+  console.log(assertEqual(result1["Fang"], 2));
+  console.log(assertEqual(result1["Agouhanna"], undefined)); //This one does not pass because is FALSE in itemsToCount  
+
+
+
+
+//Below: Tests
+
--- middle.js
+++ middle.js
@@ -27,20 +27,32 @@ const eqArrays = function(array1, array2) {
 //BELOW: Function middle
 
 
+// const middle = function(array) {
+//   let result = [];
+//   if (array.length <= 2) {
+//     return result;                                          //If the array has one or two numbers, there's no middle, return an empty array
+//   } else if (array.length > 2 && array.length % 2 !== 0) { //If the array has more than two numbers and is odd
+//     let mid = array[Math.floor(array.length / 2)];
+//     return [mid];                                             //I don't know if this is a good approach. It was returning a number, but we wanted it to be an array. So I just wrapped the variable mid in square brackets
+//   } else if (array.length > 2 && array.length % 2 === 0) { //If the array has more than two numbers and is even
+//     let mid2 = [];                                          //New array that contains two elements
+//     let mid2sub1 = array[Math.floor(array.length / 2) - 1];
+//     let mid2sub2 = array[Math.floor(array.length / 2)];
+//     mid2.push(mid2sub1, mid2sub2);
+//     return mid2;
+//   }
+// };
+
 const middle = function(array) {
   let result = [];
-  if (array.length <= 2) {
-    return result; //If the array has one or two numbers, there's no middle, return an empty array
-  } else if (array.length > 2 && array.length % 2 !== 0) { //If the array has more than two numbers and is odd
-    let mid = array[Math.floor(array.length / 2)];
-    return [mid]; //I don't know if this is a good approach. It was returning a number, but we wanted it to be an array. So I just wrapped the variable mid in square brackets
-  } else if (array.length > 2 && array.length % 2 === 0) { //If the array has more than two numbers and is even
-    let mid2 = [];                                          //New array that contains two elements
-    let mid2sub1 = array[Math.floor(array.length / 2) - 1];
-    let mid2sub2 = array[Math.floor(array.length / 2)];
-    mid2.push(mid2sub1, mid2sub2);
-    return mid2;
+  if (array.length <= 2) {                               
+  } else if (array.length > 2 && array.length % 2 !== 0) { 
+    result.push(array[Math.floor(array.length / 2)]);      
+  } else if (array.length > 2 && array.length % 2 === 0) { 
+    result.push(array[Math.floor(array.length / 2) - 1])
+    result.push(array[Math.floor(array.length / 2)])
   }
+  return result;
 };
 
 console.log(middle([1])); // []
@@ -56,4 +68,6 @@ console.log(assertArraysEqual(middle([1]), []));
 console.log(assertArraysEqual(middle([1, 2]), []));
 console.log(assertArraysEqual(middle([1, 2, 3]), [2]));
 console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]));
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]));
+
+console.log(eqArrays(middle([1, 2, 3, 4, 5]), [3])); //true
+console.log(eqArrays(middle([1, 2, 3, 4, 5]), [1])); //false
