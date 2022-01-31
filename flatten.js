1048549d30ef9abbb4af9cc95a1ac67fb7aeca10
Manuel Casanova <manucasanova@hotmail.com>
2022-01-30 19:49:22 -0800

    new function for flatten activity

--- flatten.js
+++ flatten.js
@@ -28,42 +28,40 @@ const eqArrays = function(array1, array2) {
 //Create a function flatten which will take in an array containing elements including nested arrays of elements, and return a "flattened" version of the array.
 //Given an array with other arrays inside, it can flatten it into a single-level array.
 
-//BELOW: flatten
+//BELOW: flatten using .flat method
 
-const flatten = function(array) {
-  let newArray = array.flat();
-  return newArray;
-};
-
-//The code above seems to work but it seems to simple compared to other activities. Wondering if it' s correct.
-
-//Another student suggested to do something like this, I may try:
-
-//nested for loop
-//if not array just push (ARRAY.isARRAY())
-//push array to other array
-//console.log the new array
-
-
-//Below: It returns an array with various strings separated by commas. So it's not what the exercise requires.
-//Originally it was let flattened = array.toString...etc...etc
-//Then I noticed it could be done in two lines... return array.toString...etc...etc 
-
-// const flatten = function(array) {
-//   return array.toString().split(","); //Array is transformed in String and then it splits the string in array of substrings separated by a comma
-//   // for (i = 0; i < array.length; i++) {
-//   //   if (Array.isArray(array)) {
-//   //     return flattened;
-//   //   }
-//   // }
+// const flatten = function(array)
+//   let newArray = array.flat();
+//   return newArray;
 // };
 
+//BELOW: flatten using array.isArray and push method
 
-// //BELOW: Tests
+const flatten = function(array) {                       //Flatten is a function that takes an array
+  let flatArray = [];
+  if (!Array.isArray(array)) {                          //This is just to make it nicer, printing a message if it is not an array
+    console.log('This is not an array');
+    return false;
+  }
+  for (const element of array) {                      //Loop through elements of the array
+    if (Array.isArray(element)) {                     //If the element is an Array
+      for (const subElem of element) {                 //neste loop through each part of the array
+        flatArray.push(subElem);                      //Push, add at the end of the array (which is empty) the subelement
+      }
+    } else {                                          //otherwise it pushes the element, because it's not a subarray
+      flatArray.push(element);
+    }
+  }
+  return flatArray;
+};
+
+
+//BELOW: Tests
 
 console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
 console.log(flatten([1, 2, ["three", "four"], 5, [6]])); //[1, 2, three, four, 5, 6]
 console.log(flatten([3, 2, [5, "zero"], -1, [6]]), [3, 2, 5, "zero", -1, 6]);
+console.log(flatten("hello"), [3, 2, 5, "zero", -1, 6]); //Should print "Not an array".
 
 console.log(assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]));
 console.log(assertArraysEqual(flatten([3, 2, [5, "zero"], -1, [6]]), [3, 2, 5, "zero", -1, 6]));
--- testmanuel.js
+++
@@ -1,7 +0,0 @@
-var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-var filtered = array.filter(function(value, index, arr){ 
-    return value > 5;
-});
-//filtered => [6, 7, 8, 9]
-//array => [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-
