bc56d975bd6a411bfa9d982ec8334d6c8b6c8678
Manuel Casanova <manucasanova@hotmail.com>
2022-02-15 09:36:18 -0800

    middle and tail with mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,33 @@
 const assertArraysEqual = require("../assertArraysEqual");
 const eqArrays = require("../eqArrays");
 const middle = require("../middle");
+const assert = require('chai').assert;
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+describe("#middle", () => {
+  it("returns an empty array for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it("returns an empty array for [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it("returns 2 for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns 3 for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+  it("returns true for eqArrays(middle([1, 2, 3, 4, 5]), [3])", () => {
+    assert.deepEqual(eqArrays(middle([1, 2, 3, 4, 5]), [3]), true);
+  });
+  it("returns false for eqArrays(middle([1, 2, 3, 4, 5]), [1])", () => {
+    assert.deepEqual(eqArrays(middle([1, 2, 3, 4, 5]), [1]), false);
+  });
+});
 
-eqArrays(middle([1, 2, 3, 4, 5]), [3]); //true
-eqArrays(middle([1, 2, 3, 4, 5]), [1]); //false
+// assertArraysEqual(middle([1]), []);
+// assertArraysEqual(middle([1, 2]), []);
+// assertArraysEqual(middle([1, 2, 3]), [2]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+
+// eqArrays(middle([1, 2, 3, 4, 5]), [3]); //true
+// eqArrays(middle([1, 2, 3, 4, 5]), [1]); //false
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,15 +1,32 @@
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+//const assertEqual = require('../assertEqual');
 
-//My answer
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.toString(), "Lighthouse,Labs");
-//console.log(result.toString());
+const assert = require('chai').assert;
 
-//The answer
-// Instead of comparing arrays, a workaround to this limitation could be to compare the values of the returned tail array directly, like this:
 
-const result2 = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result2.length, 2); // ensure we get back two elements
-assertEqual(result2[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result2[1], "Labs"); // ensure second element is "Labs"
+describe("#tail", () => {
+  it("returns 'Lighthouse, Labs' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
+});
+
+/*
+Note that for comparing arrays, Chai gives us assert.deepEqual, because assert.strictEqual simply uses === to compare values, which will not work for comparing objects and arrays.
+
+
+*/
+
+
+
+// //My answer
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result.toString(), "Lighthouse,Labs");
+// //console.log(result.toString());
+
+// //The answer
+// // Instead of comparing arrays, a workaround to this limitation could be to compare the values of the returned tail array directly, like this:
+
+// const result2 = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result2.length, 2); // ensure we get back two elements
+// assertEqual(result2[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result2[1], "Labs"); // ensure second element is "Labs"
