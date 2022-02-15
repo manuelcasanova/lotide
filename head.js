7aea0073240ac356747ee8ac15d018697398d2f3
Manuel Casanova <manucasanova@hotmail.com>
2022-02-15 07:34:58 -0800

    decided to go with initial idea, require both assertEqual and Head in headTest.js

--- head.js
+++ head.js
@@ -1,27 +1,12 @@
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`\uD83D\uDFE2 Assertion Passed: ${actual} === ${expected}!`);
-//   } else {
-//     console.log(`\uD83D\uDD34 Assertion Failed: ${actual} !== ${expected}!`);
-//   }
-// };
-
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-
 const head = function(array) { //Function head is going to return the 1st item of the array
   const firstItemArray = array[0];
   return firstItemArray;
 };
+
+module.exports = head;
 
 // assertEqual(head([5,6,7]), 5);   //head is going to return 5, assertEqual is going to confirm = 5
 // assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); //head is going to return "Hello", assertEqual is going to confirm = "Hello"
 // assertEqual(head(["oneElement"]), "oneElement");
 // assertEqual(head([]),);
 
-// console.log(head([5, 6, 7]));
-// console.log(head(["Hello", "Lighthouse", "Labs"]));
-// console.log(head(["oneElement"]));
-// console.log(head([]));
-
-module.exports = head;
--- test/headTest.js
+++ test/headTest.js
@@ -1,19 +1,7 @@
 const head = require('../head');
+const assertEqual = require('../assertEqual');
 
-console.log(head([5, 6, 7]));
-console.log(head(["Hello", "Lighthouse", "Labs"]));
-console.log(head(["oneElement"]));
-console.log(head([]));
-
-/*
-I'm not sure about this. 
-I made it work by having two "requires", one for head, on for assertEqual because my test code in head.js was:
-
-// assertEqual(head([5,6,7]), 5);   //head is going to return 5, assertEqual is going to confirm = 5
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); //head is going to return "Hello", assertEqual is going to confirm = "Hello"
-// assertEqual(head(["oneElement"]), "oneElement");
-// assertEqual(head([]),);
-
-If I had two "exports", one for assertEqual, one for Head it worked. But classmates pointed out that I didn't need two "requires" or "exports". So I guessed it had to do with the way I was testing. So I changed it and now it works with only one export and one require. I'm wondering if it will continue to work as I require this code from other files.
-
-*/
+assertEqual(head([5,6,7]), 5);   //head is going to return 5, assertEqual is going to confirm = 5
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); //head is going to return "Hello", assertEqual is going to confirm = "Hello"
+assertEqual(head(["oneElement"]), "oneElement");
+assertEqual(head([]),);
