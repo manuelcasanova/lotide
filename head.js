f21287bab1c74e51e66404d08cd43b2646b38198
Manuel Casanova <manucasanova@hotmail.com>
2022-02-19 17:55:34 -0800

    no new changes. Checking for errors, Found them in headTest.js

--- head.js
+++ head.js
@@ -5,6 +5,7 @@ const head = function(array) { //Function head is going to return the 1st item o
 
 module.exports = head;
 
+
 // assertEqual(head([5,6,7]), 5);   //head is going to return 5, assertEqual is going to confirm = 5
 // assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); //head is going to return "Hello", assertEqual is going to confirm = "Hello"
 // assertEqual(head(["oneElement"]), "oneElement");
